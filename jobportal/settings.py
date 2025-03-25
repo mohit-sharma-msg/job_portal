fd6bc208177fceabb0d687b183a27c4b96844578
Mohit <53290722+mohit-sharma-msg@users.noreply.github.com>
2025-03-26 00:11:10 +0530

    Update settings.py

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'jobportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'job_portal',
+        'PORT': 3306,
+        'HOST':  'localhost',
+        'USER':   'root',
+        'PASSWORD': 'root',
     }
 }
 
